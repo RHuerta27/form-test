5707c767588b7daee48832ef317585183c595a8b
RHuerta27 <ivanfix05@gmail.com>
2024-10-07 10:51:43 -0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import './loginForm.css'; 
+import React, { useState } from 'react';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('URL_DE_PRUEBA', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        setMessage('Inicio de sesión exitoso');
+      } else {
+        setMessage('Error en el inicio de sesión');
+      }
+    } catch (error) {
+      setMessage('Ocurrió un error al intentar iniciar sesión');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login-container">
+      <h2 className="login-title">Bienvenido de Nuevo</h2>
+      <form onSubmit={handleSubmit} className="login-form">
+        <div className="form-group">
+          <label htmlFor="email" className="form-label">Correo Electrónico</label>
+          <input
+            type="text"
+            id="email"
+            className="form-input"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password" className="form-label">Contraseña</label>
+          <input
+            type="password"
+            id="password"
+            className="form-input"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="login-button">Login</button>
+      </form>
+      {message && <p className="login-message">{message}</p>}
     </div>
   );
 }
